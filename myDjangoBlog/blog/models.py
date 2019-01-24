7c80108fde452d2b4ba89708ec99f2009165346d
Bong99 <bong99@gmail.com>
2019-01-24 12:38:19 +0800

    Removed Category

--- myDjangoBlog/blog/models.py
+++ myDjangoBlog/blog/models.py
@@ -5,8 +5,9 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-#    category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+    category = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
@@ -17,9 +18,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
