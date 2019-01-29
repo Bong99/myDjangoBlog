d0ffcd66a4fd9ae7fc01369897d14a22958c4e8b
Bong99 <bong99@gmail.com>
2019-01-29 16:57:03 +0800

    Fix last page no Next key
    Fix last page no next key
    Added back link in detail page

--- myDjangoBlog/blog/views.py
+++ myDjangoBlog/blog/views.py
@@ -6,9 +6,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 def post_list(request, page_id):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')[page_id*3+0:page_id*3+3]
-    #posts = posts_all[page_id*3+0:page_id*3+3]
-    page_id_next = page_id + 1
+# Change the value of postPerPage to define no of post per page
+    postPerPage = 3
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')[page_id*postPerPage+0:page_id*postPerPage+postPerPage]
+
+    page_id_next = page_id + 1;
+
+    if len(Post.objects.all())/postPerPage <= page_id+1:
+        page_id_next = 0
+
     page_id_prev = page_id - 1
     return render(request, 'blog/post_list.html', {'posts': posts, 'page_id_next':page_id_next, 'page_id_prev':page_id_prev})
 
